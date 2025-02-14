91095e786f3f7626517f1629e6f7fe4da9101f59
Wooepeke <xavier.reparon@gmail.com>
2025-02-14 22:42:22 +0100

    Added folder structure

---
+++ PiDrone/build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/src/main.cpp.o"
+  "my_program"
+  "my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PiDrone/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, CMake on Raspberry Pi!" << std::endl;
+    return 0;
+}
+
