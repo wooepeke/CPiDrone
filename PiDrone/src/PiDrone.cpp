d7928e290c0eb0a4f64032ce820e91a3daf0d293
Wooepeke <xavier.reparon@gmail.com>
2025-02-16 11:21:44 +0100

    fixed inertia functions

--- PiDrone/src/PiDrone.cpp
+++ PiDrone/src/PiDrone.cpp
@@ -1,4 +1,5 @@
 #include "PiDrone.h"
+#include <cmath>
 
 // Constructor: Initialize drone with full battery and landed state
 PiDrone::PiDrone() : batteryLevel(100), altitude(0.0), isFlying(false), eulerAngles{0.0, 0.0, 0.0}, momentsOfInertia{0.0, 0.0, 0.0} {
@@ -29,23 +30,24 @@ EulerAngles PiDrone::getDroneAngles() const {
 
 // Set MoI
 void PiDrone::setMomentsOfInertia() {
-    float l_com_x = 0.035                                                                     // Length from drone COM to the x position of the motor [m]
-    float l_com_y = 0.035                                                                     // Length from drone COM to the y position of the motor [m]
-    float l_motor = np.sqrt(l_com_x**2 + l_com_y**2)                                          // Length from drone COM to the motor [m]
-    float d_motor = 0                                                                         // Height of drone COM to motor COM [m]
-    float h_motor = 0                                                                         // Motor height [m]
-    float r_motor = 0                                                                         // Motor radius [m]
-    float m_motor = 0                                                                         // Motor mass [kg]
+    float l_com_x = 0.035;                                                                     // Length from drone COM to the x position of the motor [m]
+    float l_com_y = 0.035;                                                                     // Length from drone COM to the y position of the motor [m]
+    float l_motor = std::sqrt(std::pow(l_com_x, 2) + std::pow(l_com_y, 2));                    // Distance from drone COM to motor    
+    
+    float d_motor = 0;                                                                         // Height of drone COM to motor COM [m]
+    float h_motor = 0;                                                                         // Motor height [m]
+    float r_motor = 0;                                                                         // Motor radius [m]
+    float m_motor = 0;                                                                         // Motor mass [kg]
 
     // Moments of inertia of one motor
-    float Ixx_motor = m_motor * ((r_motor**2)/4 + (h_motor**2)/12 + d_motor**2)               // Moment of inertia of a motor around the x-axis
-    float Iyy_motor = m_motor * ((r_motor**2)/4 + (h_motor**2)/12 + l_motor**2 + d_motor**2)  // Moment of inertia of a motor around the y-axis
-    float Izz_motor = m_motor * ((r_motor**2)/2 + l_motor**2)                                 // Moment of inertia of a motor around the z-axis
-
+    float Ixx_motor = m_motor * ((std::pow(r_motor, 2) / 4) + (std::pow(h_motor, 2) / 12) + std::pow(d_motor, 2));
+    float Iyy_motor = m_motor * ((std::pow(r_motor, 2) / 4) + (std::pow(h_motor, 2) / 12) + std::pow(l_motor, 2) + std::pow(d_motor, 2));
+    float Izz_motor = m_motor * ((std::pow(r_motor, 2) / 2) + std::pow(l_motor, 2));
+    
     // Moments of inertia of the drone
-    float Ixx = 0                                                                             // Moment of inertia around the x-axis
-    float Iyy = 0                                                                             // Moment of inertia around the y-axis
-    float Izz = 0                                                                             // Moment of inertia around the z-axis    
+    float Ixx = 0;                                                                             // Moment of inertia around the x-axis
+    float Iyy = 0;                                                                             // Moment of inertia around the y-axis
+    float Izz = 0;                                                                             // Moment of inertia around the z-axis    
     return;
 }
 
